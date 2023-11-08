9de5212f99bc6d8073732c41d08e346570284d6f
martinusgalih <martinus.galih@samudrainovasi.co.id>
2023-11-09 01:07:30 +0700

    Initial Commit

---
+++ MedicalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedicalApp
+//
+//  Created by Martinus Galih Widananto on 09/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
